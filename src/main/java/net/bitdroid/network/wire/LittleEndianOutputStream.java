3d6040b603433aca645fa8d1b737f8ce0679022d
Christian Decker <decker.christian@gmail.com>
2010-12-29 15:27:30 +0100

    Added license header to all source files.

--- src/main/java/net/bitdroid/network/wire/LittleEndianOutputStream.java
+++ src/main/java/net/bitdroid/network/wire/LittleEndianOutputStream.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright 2011 Christian Decker
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * This file is part the BitDroidNetwork Project.
+ */
+
 package net.bitdroid.network.wire;
 
 import java.io.FilterOutputStream;
@@ -6,21 +24,13 @@ import java.io.OutputStream;
 import java.io.UTFDataFormatException;
 import java.nio.ByteBuffer;
 
-/*
- * @(#)LittleEndianOutputStream.java  1.0.1 99/05/19
- * 
- * Copyright 1998, 1999 Elliotte Rusty Harold
- *  
- */
 /**
  * A little endian output stream writes primitive Java numbers 
  * and characters to an output stream in a little endian format. 
  * The standard java.io.DataOutputStream class which this class
  * imitates uses big-endian integers.
  *
- * @author  Elliotte Rusty Harold
- * @version 1.0.1, 19 May 1999
- * @see     net.bitdroid.network.wire.macfaq.io.LittleEndianInputStream
+ * @see     net.bitdroid.network.wire.LittleEndianInputStream
  * @see     java.io.DataOutputStream
  */
 public class LittleEndianOutputStream extends FilterOutputStream {
